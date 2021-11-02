5eb83c8ae7c0d1271622a6af9566204a4a900884
Auliya Aqma Dinillah <auliyaaqma@gmail.com>
2021-11-02 16:23:16 +0700

    Types and Props

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,18 @@
 <template>
   <div class="app">
-    Hi, {{ name }} {{ age }}
-    <button @click="changeName('Aqma')">Change name</button>
-    <button @click="changeAge(24)">Change age</button>
-    <br>
-    <p>{{ jobs[0].title }}</p>
-    <!-- <p v-for="job in jobs" key="job.id">{{ job.title }} - {{ job.location }} - {{ job.salary }}</p> -->
+    <JobList :jobs="jobs"/> 
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref, toRefs } from 'vue';
+import JobList from './components/JobList.vue'
 import Job from './types/Job'
 
 export default defineComponent({
   name: 'App',
-  components: {},
+  components: { JobList },
   setup(){
-    // const state = reactive({
-    //   name: 'Auliya',
-    //   age: 25 as number | string
-    // })
-
-    // state.name = 999 // cannot change type
-    // state.age = '26'
-
-    // return { ...toRefs(state) }
-
-    // const name = ref('Auliya')
-    // const age = ref<number|string>(23)
-
-    // // age.value = 25 --> set value if use ref
-
-    // return { name, age}
-
     const jobs = ref<Job[]>([
         { title: 'Front End Developer', location: 'Bandung', salary: 3000000, id:'1'},
         { title: 'Back End Developer', location: 'Bandung', salary: 4000000, id:'2'},
@@ -44,34 +23,8 @@ export default defineComponent({
 
     return {jobs}
   },
-  data() {
-    return {
-      name : 'Auliya',
-      age : 23 as number | string
-    }
-  },
-  methods : {
-    changeName(name:string){
-      this.name = name
-
-      return name
-    },
-    changeAge(age:number | string){
-      this.age = age
-
-      return age
-    }
-  }
 });
 </script>
 
 <style>
-/*#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}*/
 </style>
---
+++ src/components/JobList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="job-list">
+    <ul>
+      <li v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <p>Rp. {{job.salary}}</p>
+        </div>
+        <div class="description">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut magna ipsum. Aliquam et urna quis purus ullamcorper molestie id in dui. Maecenas sit amet libero sem. Duis bibendum mauris sit amet ligula fringilla, eget gravida ligula vulputate. Cras dignissim ante ipsum. Vivamus mollis sed metus at bibendum. Integer lacinia libero sed tellus aliquam lobortis. Vestibulum odio turpis, lobortis sit amet fringilla id, maximus ultrices turpis.</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import Job from '@/types/Job'
+
+export default defineComponent({
+  props : {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>
+    }
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
