225e90f2512d1177ac2db705554d347a83709ec3
Auliya Aqma Dinillah <auliyaaqma@gmail.com>
2021-11-02 13:53:56 +0700

    Vue Component Using Typescript

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div class="app">
-    hello, hyrule
+    Hi, {{ name }} {{ age }}
+    <button @click="changeName('Aqma')"> Change name</button>
+    <button @click="changeAge(24)"> Change age</button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      name : 'Auliya',
+      age : 23 as number | string
+    }
+  },
+  methods : {
+    changeName(name:string){
+      this.name = name
+
+      return name
+    },
+    changeAge(age:number | string){
+      this.age = age
+
+      return age
+    }
   }
 });
 </script>
 
 <style>
-#app {
+/*#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
