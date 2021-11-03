490ebd5a64b01fef2707ebd55d3a1b7e395c0cab
Auliya Aqma Dinillah <auliyaaqma@gmail.com>
2021-11-03 11:39:34 +0700

    Style

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
     <header>
+      <div class="title">
+        <img src="./assets/logo.png" alt="site logo" height="50" style="margin-top: 35px;">
+        <h1>Hyrule Jobs</h1>
+      </div>
       <div class="order">
         <button @click="handleClick('title')">Order By Title</button>
         <button @click="handleClick('location')">Order By Location</button>
@@ -42,4 +46,31 @@ export default defineComponent({
 </script>
 
 <style>
+  header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
+  header .title{
+    display: flex;
+    justify-content: center;
+  }
+  header img {
+    width: 60px;
+    margin-right: 20px;
+  }
+  header h1 {
+    font-size: 3em;
+  }
 </style>
